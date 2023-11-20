27b2f34c8b378968f595dc49957b3eb37e6600c2
Jeizer-Valarezo <nakellava@hotmail.com>
2023-11-20 12:07:26 +0100

    Object Methods

---
+++ ObjectKeys.js
@@ -0,0 +1,9 @@
+const person ={
+    firstName: "Mario",
+    lastName: "Rossi",
+    age: 25,
+};
+let keys = Object.keys(person);
+console.log(keys);
+let value = Object.values(person);
+console.log(value)
