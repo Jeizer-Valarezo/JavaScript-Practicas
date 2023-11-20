aae584765bbfced403a50c611f70576c541a708e
Jeizer-Valarezo <nakellava@hotmail.com>
2023-11-20 10:50:33 +0100

    Objects Copyng

--- Persons.js
+++ Persons.js
@@ -4,8 +4,11 @@ const person1 = {
     age: 25,
   };
   
-const person2 = {...person1};
-
+const person2 = person1;
 person2.firstName ="Simon";
+
+//El ´firstName´ cambia en las 2 personas debido a que al momento de hacer una copia de un objeto en JavaScript
+//JavaScript no hace una copia como tal sino una referencia al objeto base, modificar la referencia hara lo mismo al original.
+
 console.log(person1);
 console.log(person2);
