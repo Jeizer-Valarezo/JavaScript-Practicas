ef15f661b5da024412b97980ddb2dad571c514bd
Jeizer-Valarezo <nakellava@hotmail.com>
2023-11-22 19:07:30 +0100

    sincro

--- UncompleteNotes.js
+++ UncompleteNotes.js
@@ -1,59 +1,67 @@
 function uncompletedNotes(notes) {
+  const uncompletedNotes = [];
   notes.forEach((note) => {
-    note.todos = note.todos.filter((todo) => !todo.done);
+    const uncompletedTodos = note.todos.filter((todo) => !todo.done);
+    if (uncompletedTodos.length > 0) {
+      uncompletedNotes.push({
+        id: note.id,
+        description: note.description,
+        todos: uncompletedTodos,
+      });
+    }
   });
-  return notes;
+  return uncompletedNotes;
 }
 
-  const notes = [
-    {
-      id: 1,
-      description: "Workout program",
-      todos: [
-        {
-          id: 1,
-          name: "Push ups - 10 x 3",
-          done: false,
-        },
-        {
-          id: 2,
-          name: "Abdominals - 20 x 3",
-          done: true,
-        },
-        {
-          id: 3,
-          name: "Tapis Roulant - 15min",
-          done: true,
-        },
-      ],
-    },
-    {
-      id: 2,
-      description: "Front-end Roadmap",
-      todos: [
-        {
-          id: 1,
-          name: "Learn HTML",
-          done: true,
-        },
-        {
-          id: 2,
-          name: "Learn CSS",
-          done: true,
-        },
-        {
-          id: 3,
-          name: "Learn JavaScript",
-          done: true,
-        },
-        {
-          id: 4,
-          name: "Learn Angular",
-          done: false,
-        },
-      ],
-    },
-  ];
+const notes = [
+  {
+    id: 1,
+    description: "Workout program",
+    todos: [
+      {
+        id: 1,
+        name: "Push ups - 10 x 3",
+        done: false,
+      },
+      {
+        id: 2,
+        name: "Abdominals - 20 x 3",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Tapis Roulant - 15min",
+        done: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    description: "Front-end Roadmap",
+    todos: [
+      {
+        id: 1,
+        name: "Learn HTML",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Learn CSS",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Learn JavaScript",
+        done: true,
+      },
+      {
+        id: 4,
+        name: "Learn Angular",
+        done: false,
+      },
+    ],
+  },
+];
 
-const uncompleted = uncompletedNotes(notes);
-console.log(uncompleted)
+const uncompletedNotes = uncompletedNotes(notes);
+console.log(uncompletedNotes);
