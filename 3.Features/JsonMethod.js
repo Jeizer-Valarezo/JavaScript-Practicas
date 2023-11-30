0d71b521e059590744970f72f71dd66871a89f2b
Jeizer-Valarezo <nakellava@hotmail.com>
2023-11-30 12:31:40 +0100

    JSON Methods

---
+++ 3.Features/JsonMethod.js
@@ -0,0 +1,22 @@
+//Create the fromJson method that takes in a json as parameter and returns an object of type Person.
+class Person {
+    constructor(id, firstName, lastName, age) {
+      this.id = id;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+    }
+
+    static fromJson(json){
+        let obj = JSON.parse(json);
+        
+        return new Person(obj.id,obj.firstName,obj.lastName,obj.age);
+    }
+    toJson() {
+      return JSON.stringify(this);
+    }
+  }
+  
+  const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
+  const developer = Person.fromJson(json);
+  console.log(developer);
